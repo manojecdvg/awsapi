28ed77a2f6602794a0b7b5a10cfb6043ffbf73c2
Manoj Kumar P <manoj@za.oasiscrescent.com>
2019-03-30 18:07:14 +0200

    readme.rst is added

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'vader123';
 
 /*
 |--------------------------------------------------------------------------
@@ -244,7 +244,7 @@ $config['encryption_key'] = '';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'oasis';
+$config['sess_cookie_name']		= 'phonebook';
 $config['sess_expiration']		= 32400;
 $config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= TRUE;
